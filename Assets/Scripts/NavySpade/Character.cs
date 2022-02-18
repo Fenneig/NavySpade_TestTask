78c52747fc709622a1596c2975ed2c43a9d35346
Fenneig <ninetailedkot92@gmail.com>
2022-02-18 07:15:58 +0300

    Init commit

---
+++ Assets/Scripts/NavySpade/Character.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace NavySpade
+{
+    public class Character : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Scripts/NavySpade/CharacterInputAction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+namespace NavySpade
+{
+    public class CharacterInputAction : MonoBehaviour
+    {
+        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private NavMeshAgent _agent;
+        [SerializeField] private Character _character;
+        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            
+        }
+    }
+}
